d3b5cd288ae24a69ab81821de6846fb12d7b1fa7
SuperMai <werhero@sina.cn>
2016-07-21 14:18:36 +0800

    修改包结构

---
+++ src/server/Server.java
@@ -0,0 +1,43 @@
+package server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author Maixianglei
+ * @time 2016-7-21
+ */
+public class Server {
+
+	/**
+	 * 服务端监听端口号
+	 */
+	private static final int port = 9898;
+
+	public static void main(String[] args) {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+		ServerBootstrap serverBootstrap = new ServerBootstrap();
+		serverBootstrap.group(bossGroup, workerGroup);
+		serverBootstrap.channel(NioServerSocketChannel.class);
+		serverBootstrap.childHandler(new ServerInitializer());
+
+		try {
+			// 服务器绑定端口监听
+			ChannelFuture cFuture = serverBootstrap.bind(port).sync();
+
+			// 监听服务器关闭监听，可链式编程
+			cFuture.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+
+	}
+}
